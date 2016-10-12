cef7c78385c27b80e56df60943575cf65c00344e
Peter Ebert <pebert@mpi-inf.mpg.de>
2016-10-12 18:47:35 +0200

    MAINT: copied the content of the bash script from creepiest tool; needs to be adapted for Pied Piper

--- setup/conda_env_piedpiper_default.sh
+++ setup/conda_env_piedpiper_default.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+INSTALLPATH="/TL/epigenetics2/work/pebert/conda/envs/creepiest"
+
+cd ${INSTALLPATH} &&
+mkdir -p ./etc/conda/activate.d &&
+mkdir -p ./etc/conda/deactivate.d &&
+touch ./etc/conda/activate.d/env_vars.sh &&
+touch ./etc/conda/deactivate.d/env_vars.sh
+
+CRPBINPATH="/home/pebert/work/code/mpggit/creepiest"
+CRPPYTHONPATH="/home/pebert/work/code/mpggit/creepiest"
+
+echo "#!/bin/sh" > ./etc/conda/activate.d/env_vars.sh
+
+echo "" >> ./etc/conda/activate.d/env_vars.sh
+
+echo "export PATH=${CRPBINPATH}:\$PATH" >> ./etc/conda/activate.d/env_vars.sh
+
+echo "export PYTHONPATH=${CRPPYTHONPATH}:\$PYTHONPATH" >> ./etc/conda/activate.d/env_vars.sh
+
+
