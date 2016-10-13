3325a16986cee81af87ab72bedb33c8618fc7d15
Peter Ebert <pebert@mpi-inf.mpg.de>
2016-10-13 12:58:12 +0200

    ENH: corrected YAML env file and created two shell scripts to adapt environment for DEEP setup and one for default setup

--- setup/conda_env_piedpiper_deep.sh
+++ setup/conda_env_piedpiper_deep.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# CHANGE: specify location of created Pied Piper environment
-CONDAENVPATH="${HOME}/tools/conda/env"
-INSTALLPATH="${CONDAENVPATH}/piedpiper"
+INSTALLPATH="/TL/epigenetics2/work/pebert/conda/envs/piedpiper"
 
 cd ${INSTALLPATH} &&
 mkdir -p ./etc/conda/activate.d &&
@@ -10,17 +8,8 @@ mkdir -p ./etc/conda/deactivate.d &&
 touch ./etc/conda/activate.d/env_vars.sh &&
 touch ./etc/conda/deactivate.d/env_vars.sh
 
-# CHANGE: checkout/clone location of Pied Piper git repository
-PIEDPIPERGIT="${HOME}/repos/git"
-# NOT CHANGE: these variables can be left unchanged unless you know what you are doing
-PIEDPIPERBIN="${PIEDPIPERGIT}/piedpiper/bin"
-PIEDPIPERLIB="${PIEDPIPERGIT}/piedpiper"
-
-
-#
-# Following lines:
-# Adapt environment activation script to set Pied Piper paths
-# and necessary environment variables for DRMAA/SGE
+PIEDPIPERBIN="/home/pebert/work/code/mpggit/piedpiper/bin"
+PIEDPIPERLIB="/home/pebert/work/code/mpggit/piedpiper"
 
 echo "#!/bin/sh" > ./etc/conda/activate.d/env_vars.sh
 
@@ -34,13 +23,11 @@ echo "" >> ./etc/conda/activate.d/env_vars.sh
 
 echo "export DRMAA_LIBRARY_PATH=/usr/lib/libdrmaa.so" >> ./etc/conda/activate.d/env_vars.sh
 
-echo "export SGE_ROOT=/gridware/sge" >> ./etc/conda/activate.d/env_vars.sh
+echo "export SGE_ROOT=/TL/deep-gridengine" >> ./etc/conda/activate.d/env_vars.sh
 
-echo "export SGE_CELL=default" >> ./etc/conda/activate.d/env_vars.sh
+echo "export SGE_CELL=deep" >> ./etc/conda/activate.d/env_vars.sh
 
-#
-# Following lines:
-# Unset the environment variables upon deactivation (exit of environment)
+# UNSET the environment variables upon deactivation
 
 echo "#!/bin/sh" > ./etc/conda/deactivate.d/env_vars.sh
 
